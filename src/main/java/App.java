e87109b00158dafd32348caa17937f2f5e30bccd
Andrew Malkin and Janek Brandt <andrew@>
2017-03-29 13:33:25 -0700

    add routes to admin page and post for directors and movies

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,38 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("topMovies", Movie.getTopMovies());
+      model.put("topDirectors", Director.getTopDirectors());
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/admin", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("directors", Director.all());
+      model.put("template", "templates/admin.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/directors", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      Director newDirector = new Director(name);
+      newDirector.save();
+      response.redirect("/");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/movies", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String title = request.queryParams("title");
+      int directorId = Integer.parseInt(request.queryParams("directorId"));
+      String description = request.queryParams("description");
+      String genre = request.queryParams("genre");
+      String releaseDate = request.queryParams("releaseDate");
+      Movie newMovie = new Movie(title, directorId, description, genre, releaseDate);
+      newMovie.save();
+      response.redirect("/");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
